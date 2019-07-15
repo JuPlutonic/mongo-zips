66fa6b61fe98dd022f9b1b80c1e334655a79873a
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2019-07-15 01:00:18 -1100

    PostCSS finally implemented.

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
-var postcss = require('gulp-postcss');
-var gulp = require('gulp');
-var autoprefixer = require('autoprefixer'); //Added
-var cssnano = require('cssnano'); //Added
+var concat = require('gulp-concat'),
+    atImport = require('postcss-import'),
+    cssnano = require('cssnano'),
+    cssnext = require('postcss-cssnext'),
+    gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    sourcemaps = require('gulp-sourcemaps'),
+    uglify = require('gulp-uglify');
 
-gulp.task('serve',['css'], function() {
-	gulp.watch('./public/css/*.css', ['css']);
-});
+var globs = {
+  css: [
+    'public/css/main.css'
+  ]
+};
 
-gulp.task('css', function() {
-	var plugin = [
-		autoprefixer(),
-		cssnano()
-		// PostCSS Plugins Here
-	];
-	return gulp.src('./public/css/*.css')
-		.pipe(postcss(plugin))
-		.pipe(gulp.dest('./dest'));
+gulp.task('css', function () {
+  return gulp
+    .src(globs.css)
+    .pipe(sourcemaps.init())
+    .pipe(concat('bundle.css'))
+    .pipe(postcss([
+      atImport(),
+      cssnext({
+        autoprefixer: {
+          browsers: ['IE >= 9']
+        }
+      }),
+      cssnano()
+    ]))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('bundle'));
 });
