ad9f93fb94739fcc97cf08fc264c1c9ef8939b17
JuPlutonic <8gzi0c+11ezb3n4oio0zf24zw@sharklasers.com>
2019-08-03 09:31:25 -1100

    Forms fixed. Footer added. FIXME/Rails note: application helper to add buttons on navigation bar doesn't work.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  # FIXME: nothing works
+  def active_class(controller_name, action_name)
+    # 'active' if current_page?(controller: controller_name, action: action_name)
+  end
+
+  def active_span(controller_name, action_name)
+    # '(current)' if current_page?(controller: controller_name, action: action_name)
+  end
 end
--- app/helpers/zips_helper.rb
+++ app/helpers/zips_helper.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 module ZipsHelper
-  require 'will_paginate/array'
   def to_zip(value)
     # change value to a Zip if not already a Zip
     value.is_a?(Zip) ? value : Zip.new(value)
