2c419923a399d5a4afad75870cabd28f7d8605cb
azhevlakov <azhevlakov@ceki.ru>
2021-08-11 02:23:19 +0300

    Heroku related script "deploy.sh" added - attempt #4

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,17 @@
 rm -rf dist  # Ensure that dist folder isn't exist in the first place
-git branch master heroku
+git branch master heroku # if scripts fails delete heroku with -D
+mkdir -p dist
+# git worktree add -f dist 93ee16d6e1dec888f806f44c493624e5835b7379 (hash from output `git ls-remote --heads heroku`)
 git worktree add -f dist heroku
-sed '/Gemfile\.lock/d' .gitignore > dist/.gitignore
+rm -f dist/.gitignore
+sed '/Gemfile\.lock/d;;/gems.locked/d' .gitignore > dist/.gitignore
 cp Gemfile.lock dist/Gemfile.lock
 cd dist
-git add .
+# git add .
+git add -f .gitignore
+git add -f Gemfile.lock
 git commit -m 'Deploy to Heroku'
-git push heroku master --force
+git push heroku heroku:main --force
 cd ..
 git worktree remove dist
 git branch -D heroku
